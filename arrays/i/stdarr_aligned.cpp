50bdbba767774f754870056f142b65c63b90f76f
tommygorham <tommy.gorham97@gmail.com>
2025-04-02 22:49:43 -0700

    Fix, redo stdaligned test

--- arrays/i/stdarr_aligned.cpp
+++ arrays/i/stdarr_aligned.cpp
@@ -25,64 +25,64 @@ using std::setprecision;
 int main(int argc, char* argv[])
 {
     // set type for container here   
-	using value_t = int64_t; 	
-	using index_t = int64_t; 
-	constexpr index_t size = 125000;  // NOTE: do not exceed val from ulimit -a | grep stack   		
-	std::string filename = DEFAULT_INPUT;  
-	std::string_view vt = typeid(value_t).name(); // store the datatype used to make array for results  
-	std::string_view it = typeid(index_t).name(); // store the index type used to index the array while < size
-		
+    using value_t = int64_t;    
+    using index_t = int64_t; 
+    constexpr index_t size = 250000;  
+    std::string filename = DEFAULT_INPUT;  
+    std::string_view vt = typeid(value_t).name(); // store the datatype used to make array for results  
+    std::string_view it = typeid(index_t).name(); // store the index type used to index the array while < size
+        
     // declare an array aligned to cache line   
-	alignas(64) array<value_t, size> a1;  
+    alignas(64) array<value_t, size> a1;  
      
-	// initialize and time 
-	auto s1 = std::chrono::high_resolution_clock::now(); 
+    // initialize and time 
+    auto s1 = std::chrono::high_resolution_clock::now(); 
     for (index_t i = 0; i < size; ++i) { 
         a1[i] =  i;
     } 
     auto s2 = std::chrono::high_resolution_clock::now(); 
-	std::chrono::duration<double, std::milli> init = s2-s1; 
+    std::chrono::duration<double, std::milli> init = s2-s1; 
     cout << "\nMemory Access time a[i]:  " << std::fixed << setprecision(7) << init.count() << endl; 
     
     // correctness check   
-	value_t last_element = a1[size-1]; // last element in the array 
-	value_t last_index = size-1;       // the last element's index  
-	if (last_element == last_index)     // since initialized with i  
-	{ 
-	    cout << "\na1[i] access  performed correctly." << endl; 
-	}
-	else 
-	{ 
-	    cout << "\nError in Initialization with a1[i]" << endl; 
-	    return 1; 
-	} 
+    value_t last_element = a1[size-1]; // last element in the array 
+    value_t last_index = size-1;       // the last element's index  
+    if (last_element == last_index)    // since initialized with i  
+    { 
+        cout << "\na1[i] access  performed correctly." << endl; 
+    }
+    else 
+    { 
+        cout << "\nError in Initialization with a1[i]" << endl; 
+        return 1; 
+    } 
     
-	intptr_t arr_ptr = (intptr_t)&a1[0]; // cache alignment check 
-	if ( arr_ptr % 64 == 0){ 
-		cout << "\nBeginning address is properly aligned to cache\n"; 
-	} 
+    intptr_t arr_ptr = (intptr_t)&a1[0]; // cache alignment check 
+    if ( arr_ptr % 64 == 0){ 
+        cout << "\nBeginning address is properly aligned to cache\n"; 
+    } 
 
     // prepare profiling results   
-	std::size_t bytes =  sizeof(a1); 
+    std::size_t bytes =  sizeof(a1); 
     std::size_t elements = bytes / sizeof(value_t); 
-	assert(elements == size); 
-	assert(bytes == size*sizeof(value_t)); 
-	std::string_view declaration = type_name<decltype(a1)>(); 
-	std::string_view am = ACCESSMETHOD; 
-	
+    assert(elements == size); 
+    assert(bytes == size*sizeof(value_t)); 
+    std::string_view declaration = type_name<decltype(a1)>(); 
+    std::string_view am = ACCESSMETHOD; 
+    
     // write profiling results 
-	writeToTerminal(declaration, vt, it, bytes, elements, init, am);   
-	if (argc == 2)  
+    writeToTerminal(declaration, vt, it, bytes, elements, init, am);   
+    if (argc == 2)  
     {
-	    std::string file_path(argv[1]);
-	    writeToCSV(file_path, filename, vt, it, size, bytes, init, am); 
-	}   
-	
-	// show array elements if needed     
-	if (size <= 25) 
-	{ 
-		printData(a1); 
-	}
-	
+        std::string file_path(argv[1]);
+        writeToCSV(file_path, filename, vt, it, size, bytes, init, am); 
+    }   
+    
+    // show array elements if needed     
+    if (size <= 25) 
+    { 
+        printData(a1); 
+    }
+    
     return 0; 
 }
